d8e16e8913f7f92ff1c64ce9e255a284bd165d02
dbernste5 <dbernste5@student.touro.edu>
2019-04-05 00:19:47 -0400

    created login page

--- src/LoginPage.jsx
+++ src/LoginPage.jsx
@@ -3,6 +3,51 @@ class LoginPage extends Component
     constructor(props)
     {
         super(props);
-        this.state={  };
+        this.state={  username: '', password: ''};
+    }
+
+    onChangeUserName(event)
+    {
+        this.setState({username: event.target.value});
+    }
+    onChangePassword(event)
+    {
+        this.setState( {password: event.target.value});
+    }
+    
+    onSubmitForm(event)
+    {
+        event.preventDefault();
+        fetch("/login", {
+            method: "POST",
+            body: JSON.stringify(this.state),
+            headers: {
+                "Content-Type": "application/json"
+              },
+        });
+        
+        
+        //get response
+        Response.json().then()
+
+        //see if response was true- then can log ing
+            //set up a session
+        
+    }
+
+    render()
+    {
+        return (
+            <Fragment>
+                <form id="LoginForm" onSubmit={this.onSubmitForm}>
+                    <label>Username</label><br/>
+                    <input id="username" type="text" onChange={event => this.onChangeUserName(event)}></input><br/><br/>
+                     
+                    <label>Password</label><br/>
+                    <input id="password" type="text" onChange={event => this.onChangePassword(event)}></input><br/><br/>
+                </form>
+            </Fragment>
+        )
     }
 }
+export default LoginPage
--- src/StickyView.jsx
+++ src/StickyView.jsx
@@ -5,17 +5,38 @@ class StickyView extends Component
     constructor(props)
     {
         super(props);
-        this.state = {};
+        this.state = { };
+        this.stickies = [];
+    }
+
+    componentDidMount()
+    {
+        //set the state if we decide to
+    
+        //get the logged in user, send the user ID to the fetch method to get the stickies
+        //from the session
+
+        fetchStickies();
+
+    //put the notes we get from the DB into a sticky object so we an render them for viewing
+        <li>
+        <h2></h2>   
+        <p><p>
+        </li>
+    }
+
+    fetchStickies()
+    {
+        //get the relevant stickies from the DB and map them to a list
     }
 
     render()
     {
+        //here we render the fragment as the list elements from the mapped list.
         return        (
             <Fragment>
                <ul>
-                    <li>
-                        
-                    </li>
+                    {this.stickyNotes} 
                </ul>
             </Fragment>
         )
